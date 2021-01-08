26a58a46868c74ce374da6859b277df9a78fbb11
Hakuame <hakuame@gmail.com>
2021-01-08 20:03:55 +0000

    egg timer countdown

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTime = ["Soft":5, "Medium":7, "Hard":12]
+    
+    var timer:Timer!
+    var count = 60
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        //print(eggTime[sender.currentTitle!])
+     
+        startTimer()
+        
+    
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateTime),
+            userInfo: nil,
+            repeats: true
+            )
+    }
+    
+    @objc func updateTime(){
+        print("\(count) seconds")
 
-
+        if count != 0 {
+            count -= 1
+        } else {
+            timer.invalidate()
+        }
+        
+    }
 }
